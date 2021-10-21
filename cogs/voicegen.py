686da2f1df45ec0693c16fb9216db6017f3733cd
CDE90 <cdeyolo@outlook.com>
2021-10-21 17:54:19 +0100

    Voice Generators

---
+++ cogs/voicegen.py
@@ -0,0 +1,51 @@
+import discord
+from discord.app.commands import Option
+from discord.ext import commands
+from bot import MyClient
+
+class voicegen(commands.Cog):
+
+    def __init__(self, client: MyClient):
+        self.client = client
+    
+    @commands.slash_command(description='A command to create a voice channel generator', guild_ids=[758392649979265024])
+    @commands.has_permissions(administrator=True)
+    @commands.bot_has_permissions(manage_channels=True)
+    async def voicegenerator(self, ctx: discord.ApplicationContext, category: Option(str, 'Name of generator category', required=False, default='Voice Generator'), channel: Option(str, 'Voice generator channel name', required=False, default='Voice Generator')):
+        data = self.client.jopen('Data/generator')
+
+        try:
+            data.pop(str(ctx.guild.id))
+        except:
+            pass
+
+        category = await ctx.guild.create_category(name=category)
+
+        channel = await ctx.guild.create_voice_channel(name=channel, category=category)
+
+        data[str(ctx.guild.id)] = {'cat': str(category.id), 'gen_id': str(channel.id), 'open': []}
+
+        self.client.jdump('Data/generator', data)
+
+        creation_embed = discord.Embed(color=discord.Color.green(),title='**Voice Generator Setup**', description=f'The category **{category.name}** and voice channel **{channel.name}** have been created.\n Join the voice channel to generate a voice channel.')
+        await ctx.respond(embed=creation_embed)
+
+    @commands.slash_command(description='A command to remove a voice channel generator', guild_ids=[758392649979265024])
+    @commands.has_permissions(administrator=True)
+    @commands.bot_has_permissions(manage_channels=True)
+    async def removegenerator(self, ctx: discord.ApplicationContext):
+        data = self.client.jopen('Data/generator')
+
+        try:
+            data.pop(str(ctx.guild.id))
+        except:
+            pass
+
+        self.client.jdump('Data/generator', data)
+        
+        embed = discord.Embed(color=discord.Color.green(),title='**Voice Generator Removal**', description=f'The channel will now no longer act as a voice channel generator')
+        await ctx.respond(embed=embed)
+
+
+def setup(client):
+    client.add_cog(voicegen(client))
---
+++ voicestate/generator.py
@@ -0,0 +1,42 @@
+import discord
+from bot import MyClient
+
+class generator():
+
+    def __init__(self, client: MyClient):
+        self.client = client
+
+    async def join(self, member: discord.Member, before: discord.VoiceState, after: discord.VoiceState):
+        data = self.client.jopen(f'Data/generator', str(member.guild.id))
+
+        try:
+            data[str(member.guild.id)]
+        except:
+            return
+
+        if str(after.channel.id) == data[str(member.guild.id)]['gen_id']:
+            category = self.client.get_channel(int(data[str(member.guild.id)]['cat']))
+
+            channel = await member.guild.create_voice_channel(name=f'{member.display_name}', category=category, reason='Voice Channel Generator')
+
+            data[str(member.guild.id)]['open'].append(str(channel.id))
+
+            await member.move_to(channel)
+
+            self.client.jdump('Data/generator', data)
+
+    async def leave(self, member: discord.Member, before: discord.VoiceState, after: discord.VoiceState):
+        data = self.client.jopen(f'Data/generator', str(member.guild.id))
+
+        try:
+            data[str(member.guild.id)]
+        except:
+            return
+
+        if str(before.channel.id) in data[str(member.guild.id)]['open']:
+            if len(before.channel.members) == 0:
+                await before.channel.delete()
+
+                data[str(member.guild.id)]['open'].remove(str(before.channel.id))
+
+                self.client.jdump('Data/generator', data)
