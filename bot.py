49f6b2b432cbc5e6b68522189f4190994d49a347
CDE90 <cdeyolo@outlook.com>
2021-12-01 21:05:27 +0000

    Data validation & correction

--- bot.py
+++ bot.py
@@ -49,25 +49,45 @@ class RedisUtils:
     def decode(self, s: bytes) -> str:
         return s.decode("utf-8")
 
-    def guild_add(self, guild_id: int):
-        # Guild data
-        self.r.hset(f"{guild_id}:gd", "tts:enabled", "False")
-        self.r.hset(f"{guild_id}:gd", "tts:role", "None")
-        self.r.hset(f"{guild_id}:gd", "logging", "None")
+    def guild_add(self, guild_id: int, type = None):
+        if type == None:
+            # Guild data
+            self.r.hset(f"{guild_id}:gd", "tts:enabled", "False")
+            self.r.hset(f"{guild_id}:gd", "tts:role", "None")
+            self.r.hset(f"{guild_id}:gd", "logging", "None")
 
-        # Linked data
-        self.r.hset(f"{guild_id}:linked", "voice", self.dict_to_str({}))
-        self.r.hset(f"{guild_id}:linked", "stage", self.dict_to_str({}))
-        self.r.hset(f"{guild_id}:linked", "category", self.dict_to_str({}))
-        self.r.hset(
-            f"{guild_id}:linked", "all", self.dict_to_str({"roles": [], "except": []})
-        )
-        self.r.hset(f"{guild_id}:linked", "permanent", self.dict_to_str({}))
+            # Linked data
+            self.r.hset(f"{guild_id}:linked", "voice", self.dict_to_str({}))
+            self.r.hset(f"{guild_id}:linked", "stage", self.dict_to_str({}))
+            self.r.hset(f"{guild_id}:linked", "category", self.dict_to_str({}))
+            self.r.hset(
+                f"{guild_id}:linked", "all", self.dict_to_str({"roles": [], "except": []})
+            )
+            self.r.hset(f"{guild_id}:linked", "permanent", self.dict_to_str({}))
 
-        # Generator data
-        self.r.hset(f"{guild_id}:gen", "cat", "0")
-        self.r.hset(f"{guild_id}:gen", "gen_id", "0")
-        self.r.hset(f"{guild_id}:gen", "open", self.list_to_str([]))
+            # Generator data
+            self.r.hset(f"{guild_id}:gen", "cat", "0")
+            self.r.hset(f"{guild_id}:gen", "gen_id", "0")
+            self.r.hset(f"{guild_id}:gen", "open", self.list_to_str([]))
+        elif type == 'gen':
+            # Generator data
+            self.r.hset(f"{guild_id}:gen", "cat", "0")
+            self.r.hset(f"{guild_id}:gen", "gen_id", "0")
+            self.r.hset(f"{guild_id}:gen", "open", self.list_to_str([]))
+        elif type == 'gd':
+            # Guild data
+            self.r.hset(f"{guild_id}:gd", "tts:enabled", "False")
+            self.r.hset(f"{guild_id}:gd", "tts:role", "None")
+            self.r.hset(f"{guild_id}:gd", "logging", "None")
+        elif type == 'linked':
+            # Linked data
+            self.r.hset(f"{guild_id}:linked", "voice", self.dict_to_str({}))
+            self.r.hset(f"{guild_id}:linked", "stage", self.dict_to_str({}))
+            self.r.hset(f"{guild_id}:linked", "category", self.dict_to_str({}))
+            self.r.hset(
+                f"{guild_id}:linked", "all", self.dict_to_str({"roles": [], "except": []})
+            )
+            self.r.hset(f"{guild_id}:linked", "permanent", self.dict_to_str({}))
 
         return True
 
@@ -75,26 +95,52 @@ class RedisUtils:
         self.r.delete(f"{guild_id}:gd", f"{guild_id}:linked", f"{guild_id}:gen")
 
     def get_guild_data(self, guild_id: int) -> dict:
-        data = {}
-        for key, value in self.r.hgetall(f"{guild_id}:gd").items():
-            data[self.decode(key)] = self.decode(value)
-        return data
+        r_data = self.r.hgetall(f"{guild_id}:gd")
+        if r_data:
+            data = {}
+            for key, value in r_data.items():
+                data[self.decode(key)] = self.decode(value)
+            return data
+        else:
+            self.guild_add(guild_id, 'gd')
+            r_data = self.r.hgetall(f"{guild_id}:gd")
+            data = {}
+            for key, value in r_data.items():
+                data[self.decode(key)] = self.decode(value)
+            return data
+
 
     def update_guild_data(self, guild_id: int, data: dict):
         for key in data:
             self.r.hset(f"{guild_id}:gd", key, data[key])
 
     def get_linked(self, type: str, guild_id: int) -> dict:
-        return self.str_to_dict(self.r.hget(f"{guild_id}:linked", type))
+        r_data = self.r.hget(f"{guild_id}:linked", type)
+        if r_data:
+            return self.str_to_dict(r_data)
+        else:
+            self.guild_add(guild_id, 'linked')
+            r_data = self.r.hget(f"{guild_id}:linked", type)
+            return self.str_to_dict(r_data)
 
     def update_linked(self, type: str, guild_id: int, data: dict):
         self.r.hset(f"{guild_id}:linked", type, self.dict_to_str(data))
 
     def get_generator(self, guild_id: int) -> dict:
-        data = {}
-        for key, value in self.r.hgetall(f"{guild_id}:gen").items():
-            data[self.decode(key)] = self.decode(value)
-        return data
+        r_data = self.r.hgetall(f"{guild_id}:gen")
+        if r_data:
+            data = {}
+            for key, value in r_data.items():
+                data[self.decode(key)] = self.decode(value)
+            return data
+        else:
+            self.guild_add(guild_id, 'gen')
+
+            r_data = self.r.hgetall(f"{guild_id}:gen")
+            data = {}
+            for key, value in r_data.items():
+                data[self.decode(key)] = self.decode(value)
+            return data
 
     def update_generator(self, guild_id: int, data: dict):
         for key in data:
