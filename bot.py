ffa203f928c6418720a0761348e561663004bac9
CDE <73063460+CDE90@users.noreply.github.com>
2021-12-30 20:31:53 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ default_linked = {
 default_gdata = {"tts": {"enabled": False, "role": None}, "logging": None}
 
 logger = logging.getLogger("discord")
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
 handler.setFormatter(
     logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s")
