144dd378e88a69466ee2a14d38ec6bbf13d2c920
CDE90 <cdeyolo@outlook.com>
2022-09-21 21:26:42 +0100

    Update logging

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 import asyncio
 import datetime as dt
 import json
-import logging
 import os
 
 import discord
@@ -12,17 +11,11 @@ from discord.ext import commands, tasks
 
 import config
 from utils.client import VCRolesClient
+from utils.logging import setup_logging
 from utils.utils import RedisUtils
 from views.url import TopGG
 
-logger = logging.getLogger("discord")
-logger.setLevel(logging.INFO)
-handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
-handler.setFormatter(
-    logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s")
-)
-logger.addHandler(handler)
-
+setup_logging()
 
 intents = discord.Intents(messages=True, guilds=True, reactions=True, voice_states=True)
 
---
+++ utils/logging.py
@@ -0,0 +1,93 @@
+import logging
+import os
+import sys
+from typing import Any
+
+# Code taken from discordpy/utils.py
+# https://github.com/Rapptz/discord.py/blob/c35ff4cfc637907ad797617df485f964ff26c301/discord/utils.py#L1253
+
+
+def stream_supports_colour(stream: Any) -> bool:
+    is_a_tty = hasattr(stream, "isatty") and stream.isatty()
+    if sys.platform != "win32":
+        return is_a_tty
+
+    # ANSICON checks for things like ConEmu
+    # WT_SESSION checks if this is Windows Terminal
+    # VSCode built-in terminal supports colour too
+    return is_a_tty and (
+        "ANSICON" in os.environ
+        or "WT_SESSION" in os.environ
+        or os.environ.get("TERM_PROGRAM") == "vscode"
+    )
+
+
+class _ColourFormatter(logging.Formatter):
+
+    # ANSI codes are a bit weird to decipher if you're unfamiliar with them, so here's a refresher
+    # It starts off with a format like \x1b[XXXm where XXX is a semicolon separated list of commands
+    # The important ones here relate to colour.
+    # 30-37 are black, red, green, yellow, blue, magenta, cyan and white in that order
+    # 40-47 are the same except for the background
+    # 90-97 are the same but "bright" foreground
+    # 100-107 are the same as the bright ones but for the background.
+    # 1 means bold, 2 means dim, 0 means reset, and 4 means underline.
+
+    LEVEL_COLOURS = [
+        (logging.DEBUG, "\x1b[40;1m"),
+        (logging.INFO, "\x1b[34;1m"),
+        (logging.WARNING, "\x1b[33;1m"),
+        (logging.ERROR, "\x1b[31m"),
+        (logging.CRITICAL, "\x1b[41m"),
+    ]
+
+    FORMATS = {
+        level: logging.Formatter(
+            f"\x1b[30;1m%(asctime)s\x1b[0m {colour}%(levelname)-8s\x1b[0m \x1b[35m%(name)s\x1b[0m %(message)s",
+            "%Y-%m-%d %H:%M:%S",
+        )
+        for level, colour in LEVEL_COLOURS
+    }
+
+    def format(self, record):
+        formatter = self.FORMATS.get(record.levelno)
+        if formatter is None:
+            formatter = self.FORMATS[logging.DEBUG]
+
+        # Override the traceback to always print in red
+        if record.exc_info:
+            text = formatter.formatException(record.exc_info)
+            record.exc_text = f"\x1b[31m{text}\x1b[0m"
+
+        output = formatter.format(record)
+
+        # Remove the cache layer
+        record.exc_text = None
+        return output
+
+
+def setup_logging():
+    level = logging.INFO
+    dt_fmt = "%Y-%m-%d %H:%M:%S"
+    handler = logging.StreamHandler()
+    file_handler = logging.FileHandler(filename="discord.log", mode="w")
+    file_formatter = logging.Formatter(
+        "[{asctime}] [{levelname:<8}] {name}: {message}", dt_fmt, style="{"
+    )
+    file_handler.setFormatter(file_formatter)
+
+    if isinstance(handler, logging.StreamHandler) and stream_supports_colour(
+        handler.stream
+    ):
+        formatter = _ColourFormatter()
+    else:
+        formatter = logging.Formatter(
+            "[{asctime}] [{levelname:<8}] {name}: {message}", dt_fmt, style="{"
+        )
+
+    logger = logging.getLogger()
+
+    handler.setFormatter(formatter)
+    logger.setLevel(level)
+    logger.addHandler(handler)
+    logger.addHandler(file_handler)
