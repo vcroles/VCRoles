80263290788a1034b0dd47d6d70940f33e9a9f70
CDE90 <cdeyolo@outlook.com>
2022-10-16 21:59:43 +0100

    Update base linking code

--- utils/types.py
+++ utils/types.py
@@ -1,1 +1,21 @@
+import enum
+import discord
+from prisma.models import Link
+from typing import NamedTuple, Optional
+
 DiscordID = str | int
+LinkableChannel = discord.StageChannel | discord.VoiceChannel | discord.CategoryChannel
+
+
+class LinkReturnData(NamedTuple):
+    """Return data from linking"""
+
+    status: bool
+    message: str
+    data: Optional[Link]
+
+
+class RoleCategory(enum.Enum):
+    REGULAR = 1
+    REVERSE = 2
+    STAGE_SPEAKER = 3
