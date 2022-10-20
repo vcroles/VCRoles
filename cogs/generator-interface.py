5111d115c0269a09373c08b5913f80ec36097376
Ethan Coward <ethan@ethancoward.dev>
2022-10-20 23:15:30 +0100

    Update generator interface commands

--- cogs/generator-interface.py
+++ cogs/generator-interface.py
@@ -1,8 +1,7 @@
-import json
-
 import discord
 from discord import app_commands
 from discord.ext import commands
+from prisma.models import VoiceGenerator
 
 from utils.client import VCRolesClient
 
@@ -15,44 +14,41 @@ class GenInterface(commands.Cog):
         name="interface", description="Interface commands"
     )
 
-    async def in_voice_channel(self, data, user: discord.Member) -> bool:
-
-        try:
-            user.voice
-            user.voice.channel
-        except AttributeError:
+    async def in_voice_channel(
+        self, data: VoiceGenerator, user: discord.Member
+    ) -> bool:
+        if not user.voice or not user.voice.channel or not user.voice.channel.category:
             return False
 
-        if not user.voice.channel:
+        if str(user.voice.channel.category.id) != data.categoryId:
             return False
 
-        if str(user.voice.channel.category.id) != data["cat"]:
-            return False
-
-        if str(user.voice.channel.id) == data["gen_id"]:
+        if str(user.voice.channel.id) == data.generatorId:
             return False
 
         return True
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
+        if not self.client.user or not payload.guild_id:
+            return
+
         if payload.user_id == self.client.user.id:
             return
 
         if payload.emoji.name not in ["🔒", "🔓", "🚫", "👁", "⬆", "⬇"]:
             return
 
-        data = self.client.redis.get_generator(payload.guild_id)
+        data = await self.client.db.get_generator(payload.guild_id)
 
-        try:
-            data["interface"] = json.loads(data["interface"])
-        except:
-            data["interface"] = {"channel": "0", "msg_id": "0"}
-
-        if data["interface"]["msg_id"] != str(payload.message_id):
+        if not data.interfaceMessage or data.interfaceMessage != str(
+            payload.message_id
+        ):
             return
 
         guild = self.client.get_guild(payload.guild_id)
+        if not guild:
+            return
         user = await guild.fetch_member(payload.user_id)
 
         in_voice = await self.in_voice_channel(data, user)
@@ -60,6 +56,8 @@ class GenInterface(commands.Cog):
         if not in_voice:
             try:
                 channel = self.client.get_channel(payload.channel_id)
+                if not isinstance(channel, discord.TextChannel):
+                    return
                 msg = await channel.fetch_message(payload.message_id)
                 await msg.remove_reaction(payload.emoji, user)
             except:
@@ -80,10 +78,15 @@ class GenInterface(commands.Cog):
             await self.decrease_limit(user)
 
         channel = self.client.get_channel(payload.channel_id)
+        if not isinstance(channel, discord.TextChannel):
+            return
         msg = await channel.fetch_message(payload.message_id)
         await msg.remove_reaction(payload.emoji, user)
 
     async def lock(self, user: discord.Member):
+        if not user.voice or not user.voice.channel:
+            return
+
         overwrites = user.voice.channel.overwrites
         try:
             overwrites[user.guild.default_role].connect = False
@@ -94,6 +97,9 @@ class GenInterface(commands.Cog):
         await user.voice.channel.edit(overwrites=overwrites)
 
     async def unlock(self, user: discord.Member):
+        if not user.voice or not user.voice.channel:
+            return
+
         overwrites = user.voice.channel.overwrites
         try:
             overwrites[user.guild.default_role].connect = True
@@ -104,6 +110,9 @@ class GenInterface(commands.Cog):
         await user.voice.channel.edit(overwrites=overwrites)
 
     async def hide(self, user: discord.Member):
+        if not user.voice or not user.voice.channel:
+            return
+
         overwrites = user.voice.channel.overwrites
         try:
             overwrites[user.guild.default_role].view_channel = False
@@ -114,6 +123,9 @@ class GenInterface(commands.Cog):
         await user.voice.channel.edit(overwrites=overwrites)
 
     async def unhide(self, user: discord.Member):
+        if not user.voice or not user.voice.channel:
+            return
+
         overwrites = user.voice.channel.overwrites
         try:
             overwrites[user.guild.default_role].view_channel = True
@@ -124,10 +136,20 @@ class GenInterface(commands.Cog):
         await user.voice.channel.edit(overwrites=overwrites)
 
     async def increase_limit(self, user: discord.Member):
+        if not user.voice or not user.voice.channel:
+            return
+        if not isinstance(user.voice.channel, discord.VoiceChannel):
+            return
+
         user_limit = user.voice.channel.user_limit
         await user.voice.channel.edit(user_limit=user_limit + 1)
 
     async def decrease_limit(self, user: discord.Member):
+        if not user.voice or not user.voice.channel:
+            return
+        if not isinstance(user.voice.channel, discord.VoiceChannel):
+            return
+
         user_limit = user.voice.channel.user_limit
         if user_limit > 0:
             await user.voice.channel.edit(user_limit=user_limit - 1)
@@ -135,7 +157,11 @@ class GenInterface(commands.Cog):
     @interface_commands.command(name="lock")
     async def lock_interface(self, interaction: discord.Interaction):
         """Lock your generated voice channel"""
-        data = self.client.redis.get_generator(interaction.guild_id)
+        if not interaction.guild or not isinstance(interaction.user, discord.Member):
+            return await interaction.response.send_message(
+                "You must be in a server to use this command."
+            )
+        data = await self.client.db.get_generator(interaction.guild.id)
 
         in_vc = await self.in_voice_channel(data, interaction.user)
         if not in_vc:
@@ -154,7 +180,11 @@ class GenInterface(commands.Cog):
     )
     async def unlock_interface(self, interaction: discord.Interaction):
         """Unlock your generated voice channel"""
-        data = self.client.redis.get_generator(interaction.guild_id)
+        if not interaction.guild or not isinstance(interaction.user, discord.Member):
+            return await interaction.response.send_message(
+                "You must be in a server to use this command."
+            )
+        data = await self.client.db.get_generator(interaction.guild.id)
 
         in_vc = await self.in_voice_channel(data, interaction.user)
         if not in_vc:
@@ -173,7 +203,11 @@ class GenInterface(commands.Cog):
     )
     async def hide_interface(self, interaction: discord.Interaction):
         """Hide your generated voice channel"""
-        data = self.client.redis.get_generator(interaction.guild_id)
+        if not interaction.guild or not isinstance(interaction.user, discord.Member):
+            return await interaction.response.send_message(
+                "You must be in a server to use this command."
+            )
+        data = await self.client.db.get_generator(interaction.guild.id)
 
         in_vc = await self.in_voice_channel(data, interaction.user)
         if not in_vc:
@@ -190,7 +224,11 @@ class GenInterface(commands.Cog):
     @interface_commands.command(name="show")
     async def unhide_interface(self, interaction: discord.Interaction):
         """Show your generated voice channel"""
-        data = self.client.redis.get_generator(interaction.guild_id)
+        if not interaction.guild or not isinstance(interaction.user, discord.Member):
+            return await interaction.response.send_message(
+                "You must be in a server to use this command."
+            )
+        data = await self.client.db.get_generator(interaction.guild.id)
 
         in_vc = await self.in_voice_channel(data, interaction.user)
         if not in_vc:
@@ -209,7 +247,11 @@ class GenInterface(commands.Cog):
     )
     async def increase_limit_interface(self, interaction: discord.Interaction):
         """Increase your generated voice channel user limit"""
-        data = self.client.redis.get_generator(interaction.guild_id)
+        if not interaction.guild or not isinstance(interaction.user, discord.Member):
+            return await interaction.response.send_message(
+                "You must be in a server to use this command."
+            )
+        data = await self.client.db.get_generator(interaction.guild.id)
 
         in_vc = await self.in_voice_channel(data, interaction.user)
         if not in_vc:
@@ -228,7 +270,11 @@ class GenInterface(commands.Cog):
     @interface_commands.command(name="decrease")
     async def decrease_limit_interface(self, interaction: discord.Interaction):
         """Decrease your generated voice channel user limit"""
-        data = self.client.redis.get_generator(interaction.guild_id)
+        if not interaction.guild or not isinstance(interaction.user, discord.Member):
+            return await interaction.response.send_message(
+                "You must be in a server to use this command."
+            )
+        data = await self.client.db.get_generator(interaction.guild.id)
 
         in_vc = await self.in_voice_channel(data, interaction.user)
         if not in_vc:
